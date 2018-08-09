1b0e6f3c40ece4a049df79de9284e792df8639be
Midas0615 <marekjohn006@gmail.com>
2018-08-09 10:47:17 +0100

    Update User model

--- models/user.model.js
+++ models/user.model.js
@@ -50,7 +50,7 @@ module.exports = (sequelize, DataTypes) => {
 
     Model.prototype.getJWT = function () {
         let expiration_time = parseInt(CONFIG.jwt_expiration);
-        return "Bearer "+jwt.sign({user_id:this.id}, CONFIG.jwt_encryption, {expiresIn: expiration_time});
+        return "Bearer "+jwt.sign({user_id:this.id}, CONFIG.jwt_encryption);
     };
 
     Model.prototype.toWeb = function (pw) {
